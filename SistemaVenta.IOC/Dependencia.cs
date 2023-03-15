b324b150637272e8e25b7b5102b517ea1530df70
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-15 10:16:57 -0500

    Creación de API REST y su estructura.

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.IOC
+{
+   public static class Dependencia
+    {
+        //create metodo publico para recibir servicio de conexiones  base de datos 
+    public static void inyectarDependencias(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<VntasdevContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+            });
+        }
+    }
+}
