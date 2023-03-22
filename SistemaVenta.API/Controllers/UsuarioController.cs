d2b9585be7746a3bceb3569dfea5fdec4ab9f53e
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-21 19:46:17 -0500

    Agregar Rol conmtrolller

---
+++ SistemaVenta.API/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.API.Utilidad;
+
+
+namespace SistemaVenta.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioServicio;
+
+        public UsuarioController(IUsuarioService usuarioServicio)
+        {
+            _usuarioServicio = usuarioServicio;
+        }
+        //Devolver la lista de usuarios
+
+    }
+}
---
+++ SistemaVenta.API/Utilidad/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaVenta.API.Utilidad
+{ // Add T para recibir cualquier objeto
+    public class Response<T>
+    {
+        // retornar informacion si la operacion ha sido exitosa
+        public bool status { get;set; }
+        public T value { get;set; }
+        public string msg { get;set; }
+    }
+}
