f96160f54cec88f7ed73bdfa05f6696db5d01eb8
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-21 19:59:17 -0500

    Usuaurio Controller CRUD

--- SistemaVenta.API/Controllers/UsuarioController.cs
+++ SistemaVenta.API/Controllers/UsuarioController.cs
@@ -21,5 +21,107 @@ namespace SistemaVenta.API.Controllers
         }
         //Devolver la lista de usuarios
 
+        [HttpGet]
+        [Route("Lista")]
+
+        public async Task<IActionResult> Lista()
+        {
+            var rsp = new Response<List<UsuarioDTO>>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioServicio.lista();
+            }
+            catch (Exception ex)
+            {
+
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
+        [HttpPost]
+        [Route("IniciarSesion")]
+
+        //contenedor de credenciales de usuario
+        public async Task<IActionResult> IniciarSesion([FromBody]LoginDTO login)
+        {
+            var rsp = new Response<SesionDTO>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioServicio.ValidarCredenciales(login.Correo,login.Clave);
+            }
+            catch (Exception ex)
+            {
+
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+
+        //contenedor para guardar usuario
+        public async Task<IActionResult> Guardar([FromBody] UsuarioDTO usuario)
+        {
+            var rsp = new Response<UsuarioDTO>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioServicio.Crear(usuario);
+            }
+            catch (Exception ex)
+            {
+
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+
+        //contenedor para editar usuario
+        public async Task<IActionResult> Editar([FromBody] UsuarioDTO usuario)
+        {
+            var rsp = new Response<bool>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioServicio.Editar(usuario);
+            }
+            catch (Exception ex)
+            {
+
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+
+        //contenedor para eliminar usuario
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var rsp = new Response<bool>();
+            try
+            {
+                rsp.status = true;
+                rsp.value = await _usuarioServicio.Eliminar(id);
+            }
+            catch (Exception ex)
+            {
+
+                rsp.status = false;
+                rsp.msg = ex.Message;
+            }
+            return Ok(rsp);
+        }
     }
 }
